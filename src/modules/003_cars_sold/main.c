9bf508dadabd12c1ca493de37c4f5b333c3063c7
canabal97 <martincanabal@hotmail.com>
2023-03-18 15:44:16 -0300

    Creado 003_cars_sold, modificado gitignore.

---
+++ src/modules/003_cars_sold/main.c
@@ -0,0 +1,28 @@
+//Se ingresan la cantidad de autos vendidos en los últimos 9 años. 
+//Calcular y mostrar:
+//a) El año que se vendieron la mayor cantidad de autos.
+//b) La cantidad total de autos vendidos
+
+#include <stdio.h>
+
+int main() {
+    int ventas[9];
+    int año, max_año, total_ventas, max_ventas, i;
+    
+    for(i = 0; i < 9; i++) {
+        printf("Ingrese el año donde desea ingresar el numero de ventas: ");
+        scanf("%d", &año);
+        printf("\nIngrese las ventas de autos para %d: ", año);
+        scanf("%d", &ventas[i]);
+        total_ventas += ventas[i];
+        if(ventas[i] > max_ventas) {
+            max_ventas = ventas[i];
+            max_año = año;
+        }
+    }
+    
+    printf("\n%d fue el año con mas ventas de autos, con %d ventas.\n", max_año, max_ventas);
+    printf("El total de autos vendidos es de %d.", total_ventas);
+    
+    return 0;
+}
