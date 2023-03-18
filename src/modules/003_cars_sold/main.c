0e4982b14787559108949601d60b65d269a9e1c1
canabal97 <112731537+canabal97@users.noreply.github.com>
2023-03-18 20:19:56 -0300

    Update main.c

--- src/modules/003_cars_sold/main.c
+++ src/modules/003_cars_sold/main.c
@@ -1,27 +1,27 @@
-//Se ingresan la cantidad de autos vendidos en los últimos 9 años. 
+//Se ingresan la cantidad de autos vendidos en los últimos 9 anos. 
 //Calcular y mostrar:
-//a) El año que se vendieron la mayor cantidad de autos.
+//a) El ano que se vendieron la mayor cantidad de autos.
 //b) La cantidad total de autos vendidos
 
 #include <stdio.h>
 
 int main() {
     int ventas[9];
-    int año = 0, max_año = 0, total_ventas = 0, max_ventas = 0, i;
+    int anno = 0, max_anno = 0, total_ventas = 0, max_ventas = 0, i;
     
     for(i = 0; i < 9; i++) {
-        printf("Ingrese el año donde desea ingresar el numero de ventas: ");
-        scanf("%d", &año);
-        printf("\nIngrese las ventas de autos para %d: ", año);
+        printf("Ingrese el ano donde desea ingresar el numero de ventas: ");
+        scanf("%d", &anno);
+        printf("\nIngrese las ventas de autos para %d: ", anno);
         scanf("%d", &ventas[i]);
         total_ventas += ventas[i];
         if(ventas[i] > max_ventas) {
             max_ventas = ventas[i];
-            max_año = año;
+            max_anno = anno;
         }
     }
     
-    printf("\n%d fue el año con mas ventas de autos, con %d ventas.\n", max_año, max_ventas);
+    printf("\n%d fue el ano con mas ventas de autos, con %d ventas.\n", max_anno, max_ventas);
     printf("El total de autos vendidos es de %d.", total_ventas);
     
     return 0;
