cd2d82f33ed25db16a976a890fcbe4aa91852ece
Esteban Borai <estebanborai@gmail.com>
2023-04-09 13:06:12 -0400

    fix: format

--- src/modules/001_imc/main.c
+++ src/modules/001_imc/main.c
@@ -4,18 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float cargar_peso(){
-
+float cargar_peso() {
     float peso;
 
     printf("Ingrese un peso en kgs.:\n");
     scanf("%f", &peso);
 
-    while(peso<1 || peso>500){
-
+    while(peso<1 || peso>500) {
         printf("El peso no puede se menor a 1kg ni mayor a 500kg. Intentelo nuevamente\n");
         scanf("%f", &peso);
-
     }
 
     return peso;
@@ -23,32 +20,26 @@ float cargar_peso(){
 
 
 float cargar_altura(){
-
     float altura;
 
     printf("Ingrese una altura en metros (dos decimales):\n");
     scanf("%f", &altura);
 
     while(altura<1 || altura>3){
-
         printf("La altura ingresada es incorrecta. No puede ser menor a 1 o mayor a 3 metros\n");
         scanf("%f", &altura);
-
     }
 
     return altura;
 }
 
 float calculo(float peso, float altura){
-
     float imc = peso/(altura*altura);
-
     return imc;
 }
 
 
 int main(){
-
     float peso = cargar_peso(peso);
     float altura = cargar_altura(altura);
     float imc = calculo(peso, altura);
--- src/modules/004_write_nums/main.c
+++ src/modules/004_write_nums/main.c
@@ -4,27 +4,31 @@
 
 #include <stdio.h>
 
-int main(){
-
-    int mas_menos = 0, prom = 0, cant_mult = 0, tot_mult = 0, num,  menor_num;
+int main() {
+    int mas_menos = 0;
+    int prom = 0;
+    int cant_mult = 0;
+    int tot_mult = 0;
+    int num = 0;
+    int menor_num = 0;
 
     printf("Ingresar numeros hasta que dicho numero sea negativo. El programa entonces finalizara: ");
     scanf("%d", &num);
 
     menor_num = num;
 
-    while(num>=0){
-
-        if (num>4 && num<25){
-
+    while (num >= 0) {
+        if (num > 4 && num < 25) {
             mas_menos++;
         }
-        if (num%6==0 && num!=0){//ATENCION: Sino cuenta al 0 como multiplo de 6
+
+        if (num % 6 == 0 && num != 0) {
+            // ATENCION: Sino cuenta al 0 como multiplo de 6
             cant_mult++;
             tot_mult += num;
-
         }
-        if (num<menor_num){
+
+        if (num < menor_num) {
             menor_num = num;
         }
 
@@ -34,25 +38,18 @@ int main(){
 
     printf("\nLa cantidad de numeros mayores a 4 y menores a 25 es de %d", mas_menos);
 
-    if (cant_mult>0){
-
+    if (cant_mult > 0) {
         float prom = tot_mult/cant_mult;
         printf("\nEl promedio de los numeros multiplos de 6 es de %2.f", prom);
-
-    }
-    else
-    {
+    } else {
         printf("\nNo se ingresaron numeros multiplos de 6");
     }
 
-    if (menor_num<0){
-
+    if (menor_num < 0) {
         printf("\nNo se ingreso ningun numero mayor a 0.");
     }
 
     printf("\nEl menor numero leido es de %d", menor_num);
 
-
     return 0;
-
 }
