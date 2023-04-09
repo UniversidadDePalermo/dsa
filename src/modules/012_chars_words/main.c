4b1acafc440954f8f88b53b4255c2830584a15bb
Esteban Borai <estebanborai@gmail.com>
2023-04-09 14:02:12 -0400

    feat: chars words and placeholder for filter

--- src/modules/012_chars_words/main.c
+++ src/modules/012_chars_words/main.c
@@ -6,15 +6,29 @@
 
 int main() {
   char c;
+  int char_count = 0;
+  int more_than_3 = 0;
   printf("Ingrese la frase:\n");
 
-  c = getchar();
-  putchar(c);
-
   while (c != '.') {
-    c = getchar();
-    putchar(c);
+    while(c == ' ') {
+      if (char_count > 3) {
+        more_than_3++;
+      }
+
+      char_count = 0;
+      c = getchar();
+      putchar(c);
+    }
+
+    while(c != ' ' && c != '.') {
+      char_count++;
+      c = getchar();
+      putchar(c);
+    }
   }
+
+  printf("\nLa cantidad de palabras con más de 3 caracteres es: %d", more_than_3);
 
   return 0;
 }
